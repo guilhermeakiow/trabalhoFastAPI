248e7da8c597134e38d467f2450990ba82862092
Guilherme Akiow <guilherme.akiow@gmail.com>
2025-02-16 12:38:29 -0300

    UpDate

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, status, HTTPException, Depends
 from enum import Enum
 import logging
 from groq import Groq
 import os
 from dotenv import load_dotenv
+
+API_TOKEN = 123
+
+
+# Função chamada de Token API
+def verificacaoAPI(api_token: int):
+    if api_token != API_TOKEN:
+        raise HTTPException(status_code=401, detail="API Token inválido")
+    return {"api_token": api_token}
+
+
+# Logs de Erro
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger("fastapi")
+
 
 # Carrega variáveis de ambiente do arquivo .env
 load_dotenv()
@@ -55,7 +72,7 @@ def executar_prompt_historia(estilo: str, tema: str):
     return chat_completion.choices[0].message.content  # Retorna o conteúdo gerado
 
 
-# Função para gerar sentimentos da história
+# Função para extrair sentimentos da história
 def extrator_sentimento(historia: str):
     prompt = f"Descreva os sentimentos dessa história: {historia}"
     client = Groq(
@@ -69,7 +86,7 @@ def extrator_sentimento(historia: str):
                 "content": prompt,
             }
         ],
-        model="llama-3.1-8b-instant",  # Utiliza o mesmo modelo de IA
+        model="llama-3.1-8b-instant",  # IA utilizada
     )
 
     return chat_completion.choices[
@@ -85,7 +102,7 @@ class EstiloHistoria(str, Enum):
 
 
 # Token da API para autenticação
-API_TOKEN = 123
+# API_TOKEN = 123
 
 
 # Endpoint para gerar histórias
@@ -93,20 +110,21 @@ API_TOKEN = 123
     "/v1/gerador_de_historias/{api_token}",
     summary="Retorna uma história",
     tags=["Gerador de Histórias"],
+    dependencies=[Depends(verificacaoAPI)],
 )
-def criar_história(api_token: int, Estilo: EstiloHistoria, Tema: str):
-    # Verifica se o token da API é válido
-    if api_token != API_TOKEN:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="API Token inválido"
-        )
+def criar_história(Estilo: EstiloHistoria, Tema: str):
 
     # Valida se o tema não é composto apenas por números
     if Tema.isdigit():
+        logger.error("Não é permitido números negativos")  # Log de controle no terminal
         raise HTTPException(
             status.HTTP_400_BAD_REQUEST,
             detail="Não é permitido somente números como Tema",
         )
+
+    print(
+        f"Parametro recebidos com sucesso. Estilo:{Estilo} e Tema:{Tema}"
+    )  # Log de controle no terminal
 
     # Gera a história com base no estilo e tema fornecidos
     historia = executar_prompt_historia(Estilo, Tema)
